69ff44c11ccee64646e2607809f7bbea3fb91340
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2024-10-31 09:32:17 -0300

    feat: create truples and objetcts

--- variables/main.tf
+++ variables/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region ="us-east-1"
+  region = "us-east-1"
 }
 
 //variaveis padrões
@@ -31,31 +31,51 @@ variable "mymap" {
 
 //variaveis de entrada
 variable "inputname" {
-    type = string
-    description = "Digite o nome da sua VPC"
-  
+  type        = string
+  description = "Digite o nome da sua VPC"
+
 }
 
 
 resource "aws_vpc" "myvpc" {
-    cidr_block = "10.0.0.0/16"
-  
-//Variaveis locais
-#   tags = {
-#     Name = var.mymap["key1"]
-#     Name = var.mylist[0]
-#     Name = var.vpcname
+  cidr_block = "10.0.0.0/16"
 
-#   }
+  //Variaveis locais
+  #   tags = {
+  #     Name = var.mymap["key1"]
+  #     Name = var.mylist[0]
+  #     Name = var.vpcname
 
-//variaveis de entrada
+  #   }
+
+  //variaveis de entrada
   tags = {
     Name = var.inputname
   }
 
 }
 
-
+//variaveis de saida
 output "vpcid" {
   value = aws_vpc.myvpc.id
+}
+
+
+//truplas
+variable "mytruple" {
+  type    = tuple([string, number, string])
+  default = ["cat", 1, "dog"]
+
+}
+
+//objetos
+variable "myobject" {
+  type = object({
+    name = string,
+    port = list(number),
+  })
+  default = {
+    name = "Yan"
+    port = [22, 25, 80]
+  }
 }
